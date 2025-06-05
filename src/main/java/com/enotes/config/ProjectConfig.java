236bb773ec06d50e9524943f4ac7e9ebd09a7e12
SPS <123806275+SurajPratapSingh2025@users.noreply.github.com>
2025-06-05 18:45:37 +0530

    auditing implementation

--- src/main/java/com/enotes/config/ProjectConfig.java
+++ src/main/java/com/enotes/config/ProjectConfig.java
@@ -3,6 +3,7 @@ package com.enotes.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 
 @Configuration
 public class ProjectConfig {
@@ -12,4 +13,11 @@ public class ProjectConfig {
 		
 		return new ModelMapper();
 	}
+	
+	@Bean
+	public AuditorAware<Integer> auditAware(){
+		return new AuditAwareConfig();
+	}
+	
+	
 }
