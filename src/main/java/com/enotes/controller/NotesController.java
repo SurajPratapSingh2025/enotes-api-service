a673bfe9e63062996fed149e56d302ebed5cce7b
SPS <123806275+SurajPratapSingh2025@users.noreply.github.com>
2025-06-06 04:45:06 +0530

    file download implementation

--- src/main/java/com/enotes/controller/NotesController.java
+++ src/main/java/com/enotes/controller/NotesController.java
@@ -3,10 +3,13 @@ package com.enotes.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.enotes.dto.NotesDto;
+import com.enotes.entity.FileDetails;
 import com.enotes.service.NotesService;
 import com.enotes.util.CommonUtil;
 
@@ -35,6 +39,22 @@ public class NotesController {
 		return CommonUtil.createdErrorResponseMessage("Notes not saved", HttpStatus.INTERNAL_SERVER_ERROR);
 		
 	}
+	
+	@GetMapping("/download/{id}")
+	public ResponseEntity<?> downloadFile(@PathVariable Integer id) throws Exception{
+		
+		FileDetails fileDetails = notesService.getFileDetails(id);
+		byte[] data = notesService.downloadFile(fileDetails);
+		
+		HttpHeaders headers = new HttpHeaders();
+		String contentType = CommonUtil.getContentType(fileDetails.getOriginalFileName());
+		headers.setContentType(MediaType.parseMediaType(contentType));
+		headers.setContentDispositionFormData("attachment",fileDetails.getOriginalFileName());
+		return ResponseEntity.ok().headers(headers).body(data);
+	}
+	
+	
+	
 	
 
 	@GetMapping("/")
