c94e73b50a434aa1fe324d373804fc39f251146b
SPS <123806275+SurajPratapSingh2025@users.noreply.github.com>
2025-06-05 05:32:11 +0530

    dto mapper implementation in category features

--- src/main/java/com/enotes/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/enotes/service/impl/CategoryServiceImpl.java
@@ -3,10 +3,13 @@ package com.enotes.service.impl;
 import java.util.Date;
 import java.util.List;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
+import com.enotes.dto.CategoryDto;
+import com.enotes.dto.CategoryResponse;
 import com.enotes.entity.Category;
 import com.enotes.repository.CategoryRepository;
 import com.enotes.service.CategoryService;
@@ -17,8 +20,18 @@ public class CategoryServiceImpl implements CategoryService{
 	@Autowired
 	private CategoryRepository categoryRepo;
 	
+	@Autowired
+	private ModelMapper mapper;
+	
 	@Override
-	public Boolean saveCategory(Category category) {
+	public Boolean saveCategory(CategoryDto categoryDto) {
+		
+//		Category category=new Category();
+//		category.setName(category.getName());
+//		category.setDescription(categoryDto.getDescription());
+//		category.setIsActive(categoryDto.getIsActive());
+		
+		Category category = mapper.map(categoryDto, Category.class);
 		
 		category.setIsDeleted(false);
 		category.setCreatedBy(1);
@@ -31,9 +44,23 @@ public class CategoryServiceImpl implements CategoryService{
 	}
 
 	@Override
-	public List<Category> getAllCategory() {
+	public List<CategoryDto> getAllCategory() {
 		List<Category> categories=categoryRepo.findAll();
-		return categories;
+		
+		List<CategoryDto> categoryDtoList = categories.stream().map(cat->mapper.map(cat, CategoryDto.class)).toList();
+		
+		return categoryDtoList;
 	}
+
+	@Override
+	public List<CategoryResponse> getActiveCategory() {
+		
+		List<Category> categories = categoryRepo.findByIsActiveTrue();
+		List<CategoryResponse> categoryList = categories.stream().map(cat->mapper.map(cat, CategoryResponse.class)).toList();
+		
+		return categoryList;
+	}
+
+	
 	
 }
